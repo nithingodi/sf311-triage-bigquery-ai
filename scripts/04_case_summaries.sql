c706c7aa60f1019caa1ecf1cdda8b0fc8554b9f3
nithingodi <154440857+nithingodi@users.noreply.github.com>
2025-09-10 17:36:05 -0700

    Update 04_case_summaries.sql

--- scripts/04_case_summaries.sql
+++ scripts/04_case_summaries.sql
@@ -14,42 +14,34 @@
 DECLARE project_id STRING DEFAULT 'sf311-triage-2025';
 DECLARE dataset    STRING DEFAULT 'sf311';
 
-EXECUTE IMMEDIATE FORMAT("""
-  CREATE OR REPLACE VIEW `%s.%s.batch_case_summaries` AS
-  WITH ids AS (
-    SELECT service_request_id
-    FROM `%s.%s.batch_ids`
-  ),
-  text_src AS (
-    SELECT
-      c.service_request_id,
-      cf.complaint_text,
-      q.is_bad_text
-    FROM ids c
-    JOIN `%s.%s.cases_for_classify`   AS cf USING (service_request_id)
-    JOIN `%s.%s.cases_text_quality`   AS q  USING (service_request_id)
-  ),
-  img_src AS (
-    SELECT service_request_id, summary_text AS image_summary
-    FROM `%s.%s.batch_image_summaries`
-  )
-  SELECT
-    i.service_request_id,
-    CASE
-      WHEN t.is_bad_text = FALSE THEN t.complaint_text
-      WHEN i2.image_summary IS NOT NULL THEN i2.image_summary
-      ELSE NULL
-    END AS summary,
-    CASE
-      WHEN t.is_bad_text = FALSE THEN 'text'
-      WHEN i2.image_summary IS NOT NULL THEN 'image'
-      ELSE 'none'
-    END AS summary_source
-  FROM ids i
-  JOIN text_src t USING (service_request_id)
-  LEFT JOIN img_src i2 USING (service_request_id)
-""", project_id, dataset,
-     project_id, dataset,
-     project_id, dataset,
-     project_id, dataset,
-     project_id, dataset);
+-- 04_case_summaries.sql
+CREATE OR REPLACE VIEW `sf311-triage-2025.sf311.batch_case_summaries` AS
+WITH ids AS (
+  SELECT service_request_id FROM `sf311-triage-2025.sf311.batch_ids`
+),
+text_src AS (
+  SELECT c.service_request_id, cf.complaint_text, q.is_bad_text
+  FROM ids c
+  JOIN `sf311-triage-2025.sf311.cases_for_classify` cf USING (service_request_id)
+  JOIN `sf311-triage-2025.sf311.cases_text_quality` q USING (service_request_id)
+),
+img_src AS (
+  SELECT service_request_id, summary_text AS image_summary
+  FROM `sf311-triage-2025.sf311.batch_image_summaries`
+)
+SELECT
+  i.service_request_id,
+  CASE
+    WHEN t.is_bad_text = FALSE THEN t.complaint_text
+    WHEN i2.image_summary IS NOT NULL THEN i2.image_summary
+    ELSE NULL
+  END AS summary,
+  CASE
+    WHEN t.is_bad_text = FALSE THEN 'text'
+    WHEN i2.image_summary IS NOT NULL THEN 'image'
+    ELSE 'none'
+  END AS summary_source
+FROM ids i
+JOIN text_src t USING (service_request_id)
+LEFT JOIN img_src i2 USING (service_request_id);
+
