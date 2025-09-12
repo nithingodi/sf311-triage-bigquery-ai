b5433d14e26334c4bc63b2374d3e0003aab25260
nithingodi <154440857+nithingodi@users.noreply.github.com>
2025-09-12 16:17:54 -0700

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -8,16 +8,23 @@ if [[ -z "$file" || ! -f "$file" ]]; then
   exit 2
 fi
 
-# 1) Normalize @PROJECT_ID -> ${PROJECT_ID}
-# 2) Convert projects/%s/locations/%s/connections/%s -> projects/${PROJECT_ID}/locations/${LOCATION}/connections/${CONN}
-# 3) Convert `%s.%s` -> `${PROJECT_ID}.${DATASET}`
-# 4) Normalize DECLARE lines that used "@PROJECT_ID" etc.
+# Make sure env vars are set
+: "${PROJECT_ID:?Need to set PROJECT_ID}"
+: "${DATASET:?Need to set DATASET}"
+: "${LOCATION:?Need to set LOCATION}"
+: "${CONN:?Need to set CONN}"
+
+# -----------------------------
+# Preprocess SQL
+# -----------------------------
 sed -E \
-  -e 's/@PROJECT_ID/${PROJECT_ID}/g' \
-  -e 's/projects\/%s\/locations\/%s\/connections\/%s/projects\/${PROJECT_ID}\/locations\/${LOCATION}\/connections\/${CONN}/g' \
-  -e 's/`%s\.%s\.([A-Za-z0-9_]+)`/`${PROJECT_ID}.${DATASET}.\1`/g' \
-  -e 's/`%s\.%s`/`${PROJECT_ID}.${DATASET}`/g' \
-  -e 's/%s\.%s/${PROJECT_ID}.${DATASET}/g' \
-  -e 's/DECLARE[[:space:]]+project_id[[:space:]]+STRING[[:space:]]+DEFAULT[[:space:]]+".*"/DECLARE project_id STRING DEFAULT '\''${PROJECT_ID}'\''/g' \
-  -e 's/DECLARE[[:space:]]+dataset[[:space:]]+STRING[[:space:]]+DEFAULT[[:space:]]+".*"/DECLARE dataset STRING DEFAULT '\''${DATASET}'\''/g' \
+  -e 's/@PROJECT_ID/'"$PROJECT_ID"'/g' \
+  -e 's/projects\/%s\/locations\/%s\/connections\/%s/projects\/'"$PROJECT_ID"'\/locations\/'"$LOCATION"'\/connections\/'"$CONN"'/g' \
+  -e 's/`%s\.%s\.([A-Za-z0-9_]+)`/`'"$PROJECT_ID"'.'"$DATASET"'.\1`/g' \
+  -e 's/`%s\.%s`/`'"$PROJECT_ID"'.'"$DATASET"'`/g' \
+  -e 's/%s\.%s/'"$PROJECT_ID"'.'"$DATASET"'/g' \
+  -e 's/DECLARE[[:space:]]+project_id[[:space:]]+STRING[[:space:]]+DEFAULT[[:space:]]+".*"/DECLARE project_id STRING DEFAULT \"'"$PROJECT_ID"'\"/g' \
+  -e 's/DECLARE[[:space:]]+dataset[[:space:]]+STRING[[:space:]]+DEFAULT[[:space:]]+".*"/DECLARE dataset STRING DEFAULT \"'"$DATASET"'\"/g' \
+  -e 's/DECLARE[[:space:]]+location[[:space:]]+STRING[[:space:]]+DEFAULT[[:space:]]+".*"/DECLARE location STRING DEFAULT \"'"$LOCATION"'\"/g' \
+  -e 's/DECLARE[[:space:]]+conn_id[[:space:]]+STRING[[:space:]]+DEFAULT[[:space:]]+".*"/DECLARE conn_id STRING DEFAULT \"'"$CONN"'\"/g' \
   "$file"
