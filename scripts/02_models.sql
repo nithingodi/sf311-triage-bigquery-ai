3515a2ada9c5b1b90acca6b64ca44d7daf5e41cf
nithingodi <154440857+nithingodi@users.noreply.github.com>
2025-09-12 16:42:02 -0700

    Update 02_models.sql

--- scripts/02_models.sql
+++ scripts/02_models.sql
@@ -1,13 +1,17 @@
 -- 02_models.sql
--- This script creates generative text and embeddings models
--- Env vars are substituted via Makefile + envsubst
+-- Create generative text and embedding models (remote)
+
+DECLARE project_id STRING DEFAULT "${PROJECT_ID}";
+DECLARE dataset    STRING DEFAULT "${DATASET}";
+DECLARE location   STRING DEFAULT "${LOCATION}";
+DECLARE conn_id    STRING DEFAULT "${CONN}";
 
 -- Generative text model
-CREATE OR REPLACE MODEL `${PROJECT_ID}.${DATASET}.gemini_text`
-  REMOTE WITH CONNECTION `projects/${PROJECT_ID}/locations/${LOCATION}/connections/${CONN}`
-  OPTIONS (endpoint = 'gemini-2.0-flash-001');
+CREATE OR REPLACE MODEL `${project_id}.${dataset}.gemini_text`
+REMOTE WITH CONNECTION `projects/${project_id}/locations/${location}/connections/${conn_id}`
+OPTIONS (endpoint = 'gemini-2.0-flash-001');
 
 -- Embeddings model
-CREATE OR REPLACE MODEL `${PROJECT_ID}.${DATASET}.embed_text`
-  REMOTE WITH CONNECTION `projects/${PROJECT_ID}/locations/${LOCATION}/connections/${CONN}`
-  OPTIONS (endpoint = 'text-embedding-005');
+CREATE OR REPLACE MODEL `${project_id}.${dataset}.embed_text`
+REMOTE WITH CONNECTION `projects/${project_id}/locations/${location}/connections/${conn_id}`
+OPTIONS (endpoint = 'text-embedding-005');
