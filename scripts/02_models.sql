a0b463449c50a609349114c0af0f22269cc4ebff
nithingodi <154440857+nithingodi@users.noreply.github.com>
2025-09-12 16:58:20 -0700

    Update 02_models.sql

--- scripts/02_models.sql
+++ scripts/02_models.sql
@@ -1,17 +1,19 @@
 -- 02_models.sql
--- Create generative text and embedding models (remote)
-
 DECLARE project_id STRING DEFAULT "${PROJECT_ID}";
 DECLARE dataset    STRING DEFAULT "${DATASET}";
 DECLARE location   STRING DEFAULT "${LOCATION}";
 DECLARE conn_id    STRING DEFAULT "${CONN}";
 
--- Generative text model
-CREATE OR REPLACE MODEL `${project_id}.${dataset}.gemini_text`
-REMOTE WITH CONNECTION `projects/${project_id}/locations/${location}/connections/${conn_id}`
+-- Generative text model (must include dataset)
+EXECUTE IMMEDIATE FORMAT("""
+CREATE OR REPLACE MODEL `%s.%s.gemini_text`
+REMOTE WITH CONNECTION `projects/%s/locations/%s/connections/%s`
 OPTIONS (endpoint = 'gemini-2.0-flash-001');
+""", project_id, dataset, project_id, location, conn_id);
 
 -- Embeddings model
-CREATE OR REPLACE MODEL `${project_id}.${dataset}.embed_text`
-REMOTE WITH CONNECTION `projects/${project_id}/locations/${location}/connections/${conn_id}`
+EXECUTE IMMEDIATE FORMAT("""
+CREATE OR REPLACE MODEL `%s.%s.embed_text`
+REMOTE WITH CONNECTION `projects/%s/locations/%s/connections/%s`
 OPTIONS (endpoint = 'text-embedding-005');
+""", project_id, dataset, project_id, location, conn_id);
