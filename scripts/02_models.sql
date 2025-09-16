498c44e68437049994cc5828be694de002a1d4ae
nithingodi <154440857+nithingodi@users.noreply.github.com>
2025-09-15 21:52:12 -0700

    Update 02_models.sql

--- scripts/02_models.sql
+++ scripts/02_models.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE MODEL `@@PROJECT_ID@@.@@DATASET_ID@@.gemini_text`
 REMOTE WITH CONNECTION `projects/@@PROJECT_ID@@/locations/@@LOCATION@@/connections/sf311-conn`
-OPTIONS (endpoint = 'gemini-1.5-flash');
+OPTIONS (endpoint = 'gemini-2.0-flash-001');
 
 CREATE OR REPLACE MODEL `@@PROJECT_ID@@.@@DATASET_ID@@.embed_text`
 REMOTE WITH CONNECTION `projects/@@PROJECT_ID@@/locations/@@LOCATION@@/connections/sf311-conn`
